2d8a6cd37c02fcefb64e70bd1a83de11bd771650
Luis Augusto <blackbarth@outlook.com>
2023-05-19 12:05:19 -0300

    atualizacao do projeto

--- Finances/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Finances;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- Finances/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Finances;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
